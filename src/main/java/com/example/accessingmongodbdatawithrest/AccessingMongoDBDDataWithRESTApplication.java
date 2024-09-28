f32ed6ebbe3296e3cfa62668880f9a204606f79e
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-28 16:38:26 +0200

    initial commit

---
+++ src/main/java/com/example/accessingmongodbdatawithrest/AccessingMongoDBDDataWithRESTApplication.java
@@ -0,0 +1,13 @@
+package com.example.accessingmongodbdatawithrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingMongoDBDDataWithRESTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingMongoDBDDataWithRESTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/accessingmongodbdatawithrest/AccessingMongoDbDataWithRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingmongodbdatawithrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingMongoDbDataWithRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
